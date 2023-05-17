151a84d023527bda9d1ff5f8a9d2e39d2ac6c96a
alos-source <alexander.oswald@web.de>
2023-05-17 21:20:23 +0200

    separate html from script and style

--- scripts.js
+++ scripts.js
@@ -7,11 +7,92 @@
   var bigBlind
 	var time
 	var count
+  let completedIntervalsOld
+
+    // Script refactored
+    class Timer {
+      constructor() {
+        this.intervalTime = 0;
+        this.elapsedTime = 0;
+        this.startTime = 0;
+        this.paused = true;
+        this.completedIntervals = 0;
+        this.remainingIntervalTime = 0;
+      }
+    
+      start(intervalTime) {
+        this.intervalTime = intervalTime;
+        //console.log(this.intervalTime+": Blind Interval");
+        if (this.paused) {
+          this.startTime = new Date().getTime();
+          this.remainingIntervalTime = this.intervalTime;
+          this.paused = false;
+          this.tick();
+        }
+      }
+    
+      pause() {
+        if (!this.paused) {
+          clearInterval(this.interval);
+          this.elapsedTime += new Date().getTime() - this.startTime;
+          this.paused = true;
+
+
+        }
+      }
+    
+      reset() {
+        this.intervalTime = 0;
+        this.elapsedTime = 0;
+        this.startTime = 0;
+        this.paused = true;
+        this.completedIntervals = 0;
+        this.remainingIntervalTime = 0;
+        clearInterval(this.interval);
+
+
+
+      }
+    
+      tick() {
+        this.interval = setInterval(() => {
+          this.elapsedTime = new Date().getTime() - this.startTime;
+          this.remainingIntervalTime = Math.floor((this.intervalTime - (this.elapsedTime - this.completedIntervals * this.intervalTime))/1000);
+          this.completedIntervals = Math.floor(this.elapsedTime / this.intervalTime);
+        }, 10);
+      }
+    
+      getCompletedIntervals() {
+        return this.completedIntervals;
+      }
+
+      getTimeLeft() {
+        return this.remainingIntervalTime;
+      }
+    
+      getStatus() {
+        if (this.paused) {
+          return "paused";
+        } else {
+          return "running";
+        }
+      }
+    }
+    
+
+
+  const timer = new Timer();
    
     function startTimer() {
+    //  this.completedIntervals = 0;
+
+    if (timer.completedIntervals<1){
       // Get the blinds and time from the input fields
       bigBlind = document.getElementById("big-blind").value;
       smallBlind = bigBlind/2;
+      playerValue = document.getElementById("playerValue").value;
+    }
+
       //document.getElementById("small-blind").innerHTML = smallBlind;
       time = document.getElementById("time").value;
       
@@ -25,12 +106,23 @@
 
       document.getElementById("big-blind").disabled = true;
       document.getElementById("time").disabled = true;
+      document.getElementById("playerValue").disabled = true;
+
+      // start Timer
+      timer.start(time*60*1000);
+      console.log(timer.getStatus());
+      console.log(timer.intervalTime/1000+"sec Blind Interval");
+
 
       // Get starting Time
       const d = new Date();
       let timeStart = d.toLocaleString();
       document.getElementById("timeStarted").innerHTML = timeStart;
-       
+      console.log(timeStart+": started (" + smallBlind + "$/" + bigBlind + "$)");
+      //document.getElementById("history").innerHTML=timeStart+"<br />";
+      startingBBlinds = playerValue/bigBlind;
+      document.getElementById("history").innerHTML="<br />"+timeStart+": started (" + smallBlind + "$/" + bigBlind + "$)"+" Coins pP: "+playerValue+ "$"+ " StartingBB: "+startingBBlinds+"<br />";
+
       // Set the countdown timer to the specified time in minutes
       count = time * 60;
       
@@ -53,22 +145,33 @@
         // Update the elapsed time display
         document.getElementById("elapsed").innerHTML = elapsedMinutes + ":" + elapsedSeconds;
         
+       // Update the intervals display
+       document.getElementById("intervals").innerHTML = timer.completedIntervals;
+
+       // Update the remaining time display
+      document.getElementById("remaining").innerHTML = timer.remainingIntervalTime;
+
         // Decrement the count by 1
-        count--;
+        //count--;
         
         // If the count reaches 0, double the blinds and reset the timer
-        if (count < 0) {
+        
+        if (completedIntervalsOld < timer.completedIntervals) {
           smallBlind *= 2;
           bigBlind *= 2;
-          count = time * 60;
+          //count = time * 60;
+          console.log(new Date().toLocaleString()+": BlindRaise (" + smallBlind + "$/" + bigBlind + "$)")
+          document.getElementById("history").innerHTML+=new Date().toLocaleString()+": BlindRaise (" + smallBlind + "$/" + bigBlind + "$)<br />";
 		  
+
 		// Display alert message
 		alert("time 's up - Blind Raise!");
-		console.log("new blinds: " + smallBlind + "$/" + bigBlind + "$");
+		//console.log("new blinds: " + smallBlind + "$/" + bigBlind + "$");
 		
 		  
         }
         
+        completedIntervalsOld = timer.completedIntervals;
 
         // Update the blinds display
         document.getElementById("blinds").innerHTML = smallBlind + "$/" + bigBlind + "$";
@@ -78,15 +181,23 @@
     function pauseTimer() {
       // Pause the timer by clearing the interval
       clearInterval(intervalId);
+      timer.pause(); 
+      console.log(timer.getStatus());
+
     }
     
     function resetTimer() {
       // Reset the timer, elapsed time, and blinds
       elapsed = 0;
+      completedIntervalsOld = 0;
       document.getElementById("time").disabled = false;      
       document.getElementById("big-blind").disabled = false;
+      document.getElementById("playerValue").disabled = false;
       document.getElementById("big-blind").value = "";
-
+      
+      timer.reset();
+      console.log(timer.getStatus());
 
 	  }
 	  
+
